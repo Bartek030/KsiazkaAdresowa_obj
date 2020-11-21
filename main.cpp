885e40c0af0d86120422e249bfc9806e875e85a4
Bartek <bartlomiejswies@gmail.com>
2020-11-21 16:44:09 +0100

    Created base class KsiazkaAdresowa and Uzytkownik

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include "KsiazkaAdresowa.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    KsiazkaAdresowa KsiazkaAdresowa;
+
     return 0;
 }
